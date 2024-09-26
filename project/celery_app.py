61ea5cb848eb1616f1085cd5d62a737c638c7e7f
WazedKhan <wazedkhan119399@gmail.com>
2024-09-26 23:12:38 +0600

    cleared project to make playground

--- project/celery_app.py
+++
@@ -1,8 +0,0 @@
-from celery import Celery
-
-celery_app = Celery(
-    'worker',
-    broker='redis://localhost:6379/0',
-    backend='redis://localhost:6379/0'
-)
-
--- project/tasks.py
+++
@@ -1,7 +0,0 @@
-from celery_app import celery_app
-import time
-
-@celery_app.task
-def add_numbers(a: int, b: int) -> int:
-    time.sleep(10)  # Simulate a long-running task
-    return a + b
